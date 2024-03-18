717da8109d52e9b800c33260271dfe7abfd2f2c9
NaianeReis27 <email.annereis@gmail.com>
2024-03-18 14:08:59 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,14 @@ export class AppComponent implements OnInit {
     
   }
   title = 'keypadmobile';
-  altura? = 40 
+  altura? = '40px' 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.getSize(event);
   }
 
   getSize(event: any){
-    this.altura = window.visualViewport?.height;
+    this.altura = `${window.visualViewport?.height}px`;
     console.log('Altura da tela 2:', window.visualViewport?.height,  document.documentElement.clientHeight);
   }
   
